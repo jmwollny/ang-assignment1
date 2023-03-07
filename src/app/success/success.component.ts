e6b2c49564822034d0d4e10e0c434a7d95d5f944
Jonathan Wollny <jmwollny@gmail.com>
2023-03-07 12:55:58 +0000

    add lint support and fix lint warnings

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'success-alert',
+    selector: 'app-success-alert',
     templateUrl: './success.component.html',
     styleUrls: ['./success.component.css']
 })
--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'warning-alert',
+    selector: 'app-warning-alert',
     templateUrl: './warning.component.html',
     styleUrls: ['./warning.component.css']
 })
